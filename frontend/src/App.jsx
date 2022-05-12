d47e120f1051077f3a7201314a974cfd17c1437c
ClementPremartin <clementpremartin@gmail.com>
2022-05-12 17:28:51 +0200

    replace ./ in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from "@components/Header";
-import Footer from "@components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Accueil from "./pages/Accueil";
 import Page01 from "./pages/Page01";
 import Page02 from "./pages/Page02";
