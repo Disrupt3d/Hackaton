073eb677a77d087f45bde7804629051d4e571471
Alexandre Genin <alexandre.genin.pro@gmail.com>
2022-05-12 21:01:36 +0200

    update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../assets/logo-variante.png";
 import accueildata from "../data/accueildata";
-
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
 import Boutton from "./Boutton";
 
@@ -9,7 +9,9 @@ function Header() {
   return (
     <div className="header-content">
       <img src={logo} alt="logo" width="200px" />
-      <button className="backtomenu">Retour au menu</button>
+      <Link to="/">
+        <button className="backtomenu">Retour au menu</button>
+      </Link>
     </div>
   );
 }
