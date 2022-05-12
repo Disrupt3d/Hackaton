d180f741c2009ebce95b9316baa8ac735bb57164
Guillaume-Guerit <100518697+Guillaume-Guerit@users.noreply.github.com>
2022-05-13 00:38:51 +0200

    Update missions.js

--- frontend/src/data/missions.js
+++ frontend/src/data/missions.js
@@ -15,57 +15,66 @@ const missions = [
   },
   {
     id: 2,
-    mission: "",
-    img: "",
+    mission:
+      "Ta mission, si tu l'acceptes, est d'essayer de réparer un des objets cassé de la maison, et bien évidemment demande tes parents de t'aider à le faire ! ",
+    img: "https://img.freepik.com/vecteurs-libre/pere-peut-reparer-mon-jouet_43901-31.jpg",
     type: "enfant",
   },
   {
     id: 3,
-    mission: "",
-    img: "",
+    mission:
+      "Ta mission, si tu l'acceptes, est de passer un moment avec tes parents pour vous renseigner sur les producteurs les plus proches de chez vous !",
+    img: "https://i.ibb.co/GcSddcd/renseignerfamille.jpg",
     type: "enfant",
   },
   {
     id: 4,
-    mission: "",
-    img: "",
+    mission:
+      "Ta mission, si tu l'acceptes, est qu'à l'avenir toi et ta famille, vous déplaciez à pied ou à vélo à chaque fois que c'est possible !",
+    img: "https://previews.123rf.com/images/lenm/lenm1509/lenm150900144/44984773-illustration-stickman-d-une-famille-faire-une-promenade-sur-leurs-v%C3%A9los.jpg",
     type: "enfant",
   },
   {
     id: 5,
-    mission: "",
-    img: "",
+    mission:
+      "Ta mission, si tu l'acceptes, est de commencer avec ta famille un composteur que vous pourrez remplir au fil du temps !",
+    img: "https://st.depositphotos.com/1007989/4620/i/600/depositphotos_46205411-stock-photo-compost-bin.jpg",
     type: "enfant",
   },
   {
     id: 6,
-    mission: "",
-    img: "",
-    type: "enfant",
+    mission:
+      "Ta mission, si tu l'acceptes, est d'aller ramasser les déchets laissés au sol dans l'endroit de ton choix, et bien sûr, en famille ! ",
+    img: "https://trello.com/1/cards/627d19b3b4f54d7d7b4c4e3b/attachments/627d1a3f86d910701c54c167/previews/627d1a3f86d910701c54c18d/download/enfants-ramassent-ordures-enfants-travaillent-groupe-dessin-anime-personnages-garcon-fille-enfant-trier-dechets-plastique-papier-collecter-dechets-dans-sacs-nettoyer-parc-ville_212168-1086.jpg",
+    type: "adulte",
   },
   {
     id: 7,
-    mission: "",
-    img: "",
-    type: "enfant",
+    mission:
+      "Ta mission, si tu l'acceptes, est d'essayer de faire comprendre à ton ou tes enfants ce que sont les gaz à effet de faire et en quoi ils contribuent ou réchauffement climatique !",
+    img: "http://images.jedessine.com/_uploads/_tiny_galerie/20090728/ldv-schema-rechauffement-source_eth.jpg",
+    type: "adulte",
   },
   {
     id: 8,
-    mission: "",
-    img: "",
-    type: "enfant",
+    mission:
+      "Ta mission, si tu l'acceptes, est d'aller planté des arbres en famille, beaucoup d'association le proposent de manière bénévole !",
+    img: "https://img.freepik.com/vecteurs-libre/famille-plante-arbre-conservation-nature-illustration-concept-amenagement-paysager-dans-style-plat-dessin-anime_256722-207.jpg",
+    type: "adulte",
   },
   {
     id: 9,
-    mission: "",
-    img: "",
-    type: "enfant",
+    mission:
+      "Ta mission, si tu l'acceptes, est de faire en sorte de motiver ta famille à diminuer votre consommation de viande, même un peu, c'est toujours bon à prendre !",
+    img: "https://thumbs.dreamstime.com/b/mignon-et-heureux-enfant-mange-des-fruits-de-salade-l%C3%A9gumes-159770219.jpg",
+    type: "adulte",
   },
   {
     id: 10,
-    mission: "",
-    img: "",
-    type: "enfant",
+    mission:
+      "Ta mission, si tu l'acceptes, est de prendre l'habitude de mettre le chauffage juste un peu moins fort, quitte à rajouter une petite couche de vêtement !",
+    img: "http://jwhy.fr/client/lagoa/anthonyhab/img/Anthony-Habitat-guide-chauffage-illus-conveteur.png",
+    type: "adulte",
   },
   {
     id: 11,
