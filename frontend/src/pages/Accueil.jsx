02a72e5abda6beb5f5f16591d918bb4e378db973
Alexandre Genin <alexandre.genin.pro@gmail.com>
2022-05-12 11:19:39 +0200

    adding css

--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -7,6 +7,14 @@ function Accueil() {
   return (
     <div>
       <h1>Accueil</h1>
+      <div className="test"></div>
+      <button className="btn-test">Testez moi ! </button>
+      <p className="p-test">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
+        placeat laborum assumenda at nihil fugiat rem, consequatur quasi
+        consequuntur nemo dolor libero aliquam illo, voluptas neque odit saepe
+        eaque facilis.
+      </p>
     </div>
   );
 }
