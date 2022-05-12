3a51879864d2ae1daefe8566214109314efe220f
GUILLAUME-GUERIT <guillaumeguerit@gmail.com>
2022-05-12 18:00:51 +0200

    fix context

--- frontend/src/contexts/UserContext.jsx
+++ frontend/src/contexts/UserContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
-function ContextProvider({ children }) {
+export function ContextProvider({ children }) {
   const [user, setUser] = useState([]);
   return (
     <UserContext.Provider value={{ user, setUser }}>
