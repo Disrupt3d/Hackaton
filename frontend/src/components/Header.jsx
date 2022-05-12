000802bc915e03767cb90906580010c40b8e0d59
Alexandre Genin <alexandre.genin.pro@gmail.com>
2022-05-12 22:55:21 +0200

    update css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <div className="header-content">
       <img src={logo} alt="logo" width="200px" />
-      <Link to="/">
+      <Link to="/page01">
         <button className="backtomenu" type="button">
           Retour au menu
         </button>
